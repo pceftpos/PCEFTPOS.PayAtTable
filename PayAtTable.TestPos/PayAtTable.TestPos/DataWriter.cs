83673397627b3adf0e96699806d85e2887b9fb8d
pceftpos-dev <jcorrea@pceftpos.com.au>
2018-05-04 15:49:27 +1000

    1.9.0.0 Release

---
+++ PayAtTable.TestPos/PayAtTable.TestPos/DataWriter.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayAtTable.TestPos
+{
+    public class JsonWriter 
+    {
+        string GetPath(string filename)
+        {
+            var path = $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\\PC-EFTPOS\\";
+            Directory.CreateDirectory(path);
+
+            var fullPath = $"{path}PAT-{filename}";
+           
+            return fullPath;
+        }
+
+        public bool Load<T>(string filename, out T items)
+        {
+            try
+            {
+                string contents = File.ReadAllText(GetPath(filename));
+                items = (T)JsonConvert.DeserializeObject(contents, typeof(T));
+
+                return (items != null);
+            }
+            catch
+            {
+                items = default(T);
+            }
+
+            return false;
+        }
+
+        public bool Save<T>(T items, string filename)
+        {
+            try
+            {
+                string json = JsonConvert.SerializeObject(items, Formatting.Indented);
+                File.WriteAllText(GetPath(filename), json);
+
+                return true;
+            }
+            catch
+            {
+            }
+
+            return false;
+        }
+    }
+}
